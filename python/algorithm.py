75b66a27577a05018247c8cc821d608ced690d52
zhanfeng.zhang <zhanfeng.zhang@xqchuxing.com>
2018-07-24 16:33:56 +0800

    添加 递归二分/递归求和/阶乘/递归最大值

--- python/algorithm.py
+++ python/algorithm.py
@@ -16,6 +16,27 @@ def binary_search(sorted_list, value):
             high = mid - 1
     return None
 
+
+def recursion_binary_search(sorted_list, value, start, end):
+    if (len(sorted_list) == 0):
+        return None
+    if (start == end - 1):
+        if (sorted_list[start] == value):
+            return start
+        else:
+            return None
+    elif (start > end - 1):
+        return None
+
+    mid_index = int((start + end - 1) / 2)
+    mid_value = sorted_list[mid_index]
+    if (mid_value == value):
+        return mid_index
+    elif (mid_value < value):
+        return recursion_binary_search(sorted_list, value, mid_index + 1, end)
+    else:
+        return recursion_binary_search(sorted_list, value, start, mid_index)
+
 def selection_sort(ordinary_list):
     """
         选择排序
@@ -29,6 +50,7 @@ def selection_sort(ordinary_list):
                 max_index = search_index
         _swap(ordinary_list, index, max_index)
 
+
 def _swap(list, index_1, index_2):
     """
     交换列表中的2个元素
@@ -38,5 +60,61 @@ def _swap(list, index_1, index_2):
     tmp = list[index_1]
     list[index_1] = list[index_2]
     list[index_2] = tmp
-    
 
+
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    return n * factorial(n-1)
+
+
+def factorial2(n):
+    ret = 1
+    for i in range(1, n + 1):
+        ret *= i
+    return ret
+
+
+def recursion_sum(lst=[]):
+    """
+    采用递归方法求和
+    """
+    lng = len(lst)
+    if (lng == 0):
+        return 0
+    elif (lng == 1):
+        return lst[0]
+    return lst.pop() + recursion_sum(lst)
+
+
+def recursion_sum2(lst=[], start=0, stop=0):
+    """
+    采用递归方法求和
+    """
+    if (len(lst) == 0):
+        return 0
+    lng = stop - start
+    if (lng <= 0):
+        return 0
+    elif (lng == 1):
+        return lst[start]
+    return lst[start] + recursion_sum2(lst, start + 1, stop)
+
+
+def recursion_max_value(lst=[], start=0, end=0):
+    """
+    递归算出列表中的最大值
+    """
+    if (len(lst) == 0):
+        return None
+    lng = end - start
+    if (lng <= 0):
+        return None
+    if (lng == 1):
+        return lst[start]
+
+    guess = recursion_max_value(lst, start + 1, end)
+    if (guess > lst[start]):
+        return guess
+    else:
+        return lst[start]
--- python/test_algorithm.py
+++ python/test_algorithm.py
@@ -1,6 +1,13 @@
 import unittest as ut
 from algorithm import binary_search
+from algorithm import recursion_binary_search
 from algorithm import selection_sort
+from algorithm import factorial
+from algorithm import factorial2
+from algorithm import recursion_sum
+from algorithm import recursion_sum2
+from algorithm import recursion_max_value
+
 class AlgorithmTestCase(ut.TestCase):
 
     def test_binary_search(self):
@@ -17,6 +24,54 @@ class AlgorithmTestCase(ut.TestCase):
 
     def test_selection_sort(self):
         list = [1, 5, 9, 7, 11, 55]
-        
+
         selection_sort(list)
         self.assertListEqual([55, 11, 9, 7, 5, 1], list)
+
+    def test_factorial(self):
+        self.assertEqual(120, factorial(5))
+        self.assertFalse(120 == factorial(4))
+
+    def test_factorial2(self):
+        self.assertEqual(24, factorial2(4))
+
+    def test_recursion_sum(self):
+        array = []
+        self.assertEqual(0, recursion_sum(array))
+
+        array = [1]
+        self.assertEqual(1, recursion_sum(array))
+
+        array = [1,2,3,4,5]
+        self.assertEqual(15, recursion_sum2(array, 0, len(array)))
+
+    def test_recursion_sum2(self):
+        array = []
+        self.assertEqual(0, recursion_sum2(array, 0, len(array)))
+
+        array = [1]
+        self.assertEqual(1, recursion_sum2(array, 0, len(array)))
+
+        array = [1,2,3,4,5]
+        self.assertEqual(15, recursion_sum2(array, 0, len(array)))
+
+        array = [1,2,3,4,5]
+        self.assertEqual(9, recursion_sum2(array, len(array) - 2, len(array)))
+    
+    def test_recursion_max_value(self):
+        array = []
+        self.assertEqual(None, recursion_max_value(array))
+
+        array = [5]
+        self.assertEqual(5, recursion_max_value(array, 0, len(array)))
+
+        array = [5, 4, 1, 11]
+        self.assertEqual(11, recursion_max_value(array, 0, len(array)))
+
+    def test_recursion_binary_search(self):
+        sorted_list = [1, 3, 5, 7, 9]
+        for index in range(len(sorted_list)):
+            self.assertEqual(index, recursion_binary_search(sorted_list, sorted_list[index], 0, len(sorted_list)))
+
+        self.assertEqual(None, recursion_binary_search(sorted_list, -1, 0, len(sorted_list)))
+        self.assertEqual(None, recursion_binary_search(sorted_list, 10, 0, len(sorted_list)))
