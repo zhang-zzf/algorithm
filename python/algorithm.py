b54b0be9e829f241ceceadb4ed9c106d5b0b26a8
zhanfeng.zhang <zhanfeng.zhang@xqchuxing.com>
2018-07-24 10:10:41 +0800

    add select sort

--- python/algorithm.py
+++ python/algorithm.py
@@ -15,3 +15,28 @@ def binary_search(sorted_list, value):
         else:
             high = mid - 1
     return None
+
+def selection_sort(ordinary_list):
+    """
+        选择排序
+    """
+
+    lng = len(ordinary_list)
+    for index in range(lng):
+        max_index = index
+        for search_index in range(index + 1, lng):
+            if (ordinary_list[search_index] > ordinary_list[max_index]):
+                max_index = search_index
+        _swap(ordinary_list, index, max_index)
+
+def _swap(list, index_1, index_2):
+    """
+    交换列表中的2个元素
+    """
+    if (index_1 == index_2):
+        return None
+    tmp = list[index_1]
+    list[index_1] = list[index_2]
+    list[index_2] = tmp
+    
+
--- python/test_algorithm.py
+++ python/test_algorithm.py
@@ -1,16 +1,22 @@
 import unittest as ut
 from algorithm import binary_search
-
-class Algorithm(ut.TestCase):
+from algorithm import selection_sort
+class AlgorithmTestCase(ut.TestCase):
 
     def test_binary_search(self):
-        sorted_list = [1,3,5,7,9]
+        sorted_list = [1, 3, 5, 7, 9]
 
         index = binary_search(sorted_list, 1)
         self.assertEqual(0, index)
 
         index = binary_search(sorted_list, 7)
         self.assertEqual(3, index)
-        
+
         index = binary_search(sorted_list, 10)
         self.assertEqual(None, index)
+
+    def test_selection_sort(self):
+        list = [1, 5, 9, 7, 11, 55]
+        
+        selection_sort(list)
+        self.assertListEqual([55, 11, 9, 7, 5, 1], list)
