f58ac3b66e72b0479170de3610a3c816f9c11fef
zhanfeng.zhang <zhanfeng.zhang@xqchuxing.com>
2018-07-25 14:14:53 +0800

    java 添加 选择排序/快速排序（递归实现）/快速排序（非递归实现）/最大公约数（递归实现）/最大公约数（非递归实现）

--- java/src/main/java/Algorithm.java
+++ java/src/main/java/Algorithm.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Algorithm {
 
     /**
@@ -21,5 +24,156 @@ public class Algorithm {
             }
         }
         return -1;
+    }
+
+    /**
+     * 选择排序
+     *
+     * @param list
+     */
+    public static void selectionSort(int[] list) {
+        for (int index = 0; index < list.length; index++) {
+            int maxValueIndex = index;
+            for (int searchIndex = index + 1; searchIndex < list.length; searchIndex++) {
+                if (list[searchIndex] > list[maxValueIndex]) {
+                    maxValueIndex = searchIndex;
+                }
+            }
+            _swap(list, index, maxValueIndex);
+        }
+    }
+
+    /**
+     * 快速排序（递归实现）
+     *
+     * @param array
+     */
+    public static void recursionQuickSort(int[] array, int start, int end) {
+        int low = start, high = end - 1;
+        // 基线条件
+        if (low >= high) {
+            return;
+        }
+        int index = _partSort(array, low, high);
+        recursionQuickSort(array, start, index);
+        recursionQuickSort(array, index + 1, end);
+    }
+
+    /**
+     * 快速排序算法
+     *
+     * @param array
+     */
+    public static void quickSort(int[] array, int start, int end) {
+        Deque<Integer> stack = new LinkedList<>();
+        int low = start, high = end -1;
+        stack.push(high);
+        stack.push(low);
+        while (stack.size() > 0) {
+            low = stack.poll();
+            high = stack.poll();
+            int index = _partSort2(array, low, high);
+            if (low < index - 1) {
+                stack.push(index -1);
+                stack.push(low);
+            }
+            if (index + 1 < high) {
+                stack.push(high);
+                stack.push(index + 1);
+            }
+        }
+    }
+
+    /**
+     * 最大公约数递归实现（欧几里德算法）
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int recursionGcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+        return recursionGcd(b, a % b);
+    }
+
+    /**
+     * 最大公约数（欧几里德算法）
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcd(int a, int b) {
+        for (int r = a % b; r != 0; r = a % b) {
+            a = b;
+            b = r;
+        }
+        return b;
+    }
+
+    private static int _partSort(int[] array, int low, int high) {
+        int index = low;
+        int pivot = array[index];
+
+        while (low < index || high > index) {
+            while (low < index) {
+                if (array[low] > pivot) {
+                    _swap(array, low, index);
+                    index = low;
+                } else {
+                    low += 1;
+                }
+            }
+            while (high > index) {
+                if (array[high] < pivot) {
+                    _swap(array, index, high);
+                    index = high;
+                } else {
+                    high -= 1;
+                }
+            }
+        }
+        return index;
+    }
+
+    /**
+     * better than _partSort
+     *
+     * @param array
+     * @param left
+     * @param right
+     * @return
+     */
+    private static int _partSort2(int[] array, int left, int right) {
+
+        int pivotIndex = right;
+        int pivot = array[pivotIndex];
+        while (left < right) {
+            while (left < right && array[left] <= pivot) {
+                left += 1;
+            }
+            while (left < right && array[right] >= pivot) {
+                right -= 1;
+            }
+            _swap(array, left, right);
+        }
+        _swap(array, left, pivotIndex);
+
+        return left;
+    }
+
+
+    private static void _swap(int[] list, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        int tmp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = tmp;
     }
 }
--- java/src/test/java/AlgorithmTest.java
+++ java/src/test/java/AlgorithmTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class AlgorithmTest {
 
@@ -15,4 +16,36 @@ public class AlgorithmTest {
         index = Algorithm.binarySearch(sortedList, 3);
         assertEquals(1, index);
     }
+
+    @Test
+    public void selectionSort() {
+        int[] list = {1, 8, 3, 9, 11};
+        Algorithm.selectionSort(list);
+        assertArrayEquals(new int[]{11, 9, 8, 3, 1}, list);
+    }
+
+    @Test
+    public void recursionQuickSort() {
+        int[] list = {1, 8, 3, 9, 1, 100, 88, -50, -100};
+        Algorithm.recursionQuickSort(list, 0, list.length);
+        assertArrayEquals(new int[]{ -100, -50, 1, 1, 3, 8, 9, 88, 100}, list);
+    }
+
+    @Test
+    public void quickSort() {
+        int[] list = {1, 8, 3, 9, 1, 100, 88, -50, -100};
+        Algorithm.quickSort(list, 0, list.length);
+        assertArrayEquals(new int[]{ -100, -50, 1, 1, 3, 8, 9, 88, 100}, list);
+    }
+
+    @Test
+    public void gcd() {
+        int gcd = Algorithm.recursionGcd(100, 8888);
+        assertEquals(4, gcd);
+        assertEquals(gcd, Algorithm.recursionGcd(8888, 100));
+        assertEquals(gcd, Algorithm.gcd(100, 8888));
+        assertEquals(gcd, Algorithm.gcd(8888, 100));
+    }
+
+
 }
